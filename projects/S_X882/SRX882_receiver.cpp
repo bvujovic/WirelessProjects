8f60c9ee6289e06634869c8f4f9700df1dc41586
bvujovic <bv.net@outlook.com>
2021-04-18 02:55:18 +0200

    Created project(s) folder structure. Imported old S*X882 test project w docs folder: code, pics, docs.

---
+++ projects/S_X882/SRX882_receiver.cpp
@@ -0,0 +1,39 @@
+#include <RH_ASK.h>
+#include <SPI.h>
+RH_ASK rf;
+long ms;
+
+const byte pinLed = LED_BUILTIN;
+void ledOn(bool on) { digitalWrite(pinLed, on); }
+
+void setup()
+{
+  rf.init();
+  Serial.begin(9600);
+  Serial.println("Started.");
+  pinMode(pinLed, OUTPUT);
+}
+
+void loop()
+{
+  uint8_t buf[10];
+  uint8_t buflen;
+
+  ms = millis();
+  if (rf.recv(buf, &buflen))
+  {
+    ledOn(true);
+    String s = String((char *)buf);
+    Serial.println(s.substring(0, buflen));
+    delay(5);
+    Serial.println(millis() - ms);
+    delay(100);
+    ledOn(false);
+    // Serial.println(s);
+    //    int x = buf[0] * 256 + buf[1];
+    //    Serial.println(x);
+    //    Serial.print("buflen: "); Serial.println(buflen);
+  }
+  else
+    delay(100);
+}
---
+++ src/STX882_transmitter.cpp
@@ -0,0 +1,36 @@
+#include <RH_ASK.h>
+RH_ASK rf;
+//#include <SPI.h>
+
+const byte pinLed = LED_BUILTIN;
+void ledOn(bool on) { digitalWrite(pinLed, on); }
+
+void setup()
+{
+  rf.init();
+  pinMode(pinLed, OUTPUT);
+  // Serial.begin(9600);
+  //  digitalWrite(LED_BUILTIN, false);
+}
+
+void loop()
+{
+  //  //B const char *msg = "...!";
+  //  int secs = millis() / 1000;
+  //  //T Serial.println(secs);
+  //  char msg[7];
+  //  itoa(secs, msg, 10); // int, string buffer, base
+  //  // Serial.print((byte)msg[0]); Serial.print(" - "); Serial.println((int)msg[1]);
+  //  // Serial.print(msg); Serial.print(" - "); Serial.println(strlen(msg));
+  //  rf.send((uint8_t*)msg, strlen(msg));
+  //  rf.waitPacketSent();
+
+  char msg[] = {'a', 0};
+  rf.send((uint8_t *)msg, 2);
+  rf.waitPacketSent();
+
+  ledOn(true);
+  delay(200);
+  ledOn(false);
+  delay(4800);
+}
